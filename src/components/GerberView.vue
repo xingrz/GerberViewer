32da5e161629c961310e003b6180844449880c75
XiNGRZ <hi@xingrz.me>
2022-09-26 14:32:12 +0800

    Handle scroll and scale events

--- src/components/GerberView.vue
+++ src/components/GerberView.vue
@@ -7,7 +7,7 @@
 <script lang="ts" setup>
 import type { InputLayer } from 'pcb-stackup';
 import stackup from 'pcb-stackup';
-import { defineProps, ref, watch } from 'vue';
+import { defineProps, reactive, ref, watch } from 'vue';
 
 import { loadSvgImage } from '@/utils/svg';
 import { scaleInside } from '@/utils/graphic';
@@ -26,6 +26,9 @@ watch(props, async () => {
 const containerRef = ref<HTMLElement>();
 const canvasRef = ref<HTMLCanvasElement>();
 
+const scale = ref(1);
+const scroll = reactive({ x: 0, y: 0 });
+
 function draw(): void {
   const container = containerRef.value;
   const canvas = canvasRef.value;
@@ -39,11 +42,42 @@ function draw(): void {
   if (!ctx) return;
 
   const rect = scaleInside(canvas, image.value);
-  ctx.drawImage(image.value, rect.x, rect.y, rect.width, rect.height);
+
+  ctx.save();
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.drawImage(
+    image.value,
+    -rect.width / 2 * scale.value + scroll.x,
+    -rect.height / 2 * scale.value + scroll.y,
+    rect.width * scale.value,
+    rect.height * scale.value
+  );
+  ctx.restore();
 }
 
 window.addEventListener('resize', draw);
-watch(image, draw);
+watch(image, () => {
+  scroll.x = 0;
+  scroll.y = 0;
+  draw();
+});
+
+watch(canvasRef, () => {
+  const canvas = canvasRef.value;
+  if (!canvas) return;
+
+  canvas.addEventListener('wheel', (evt) => {
+    evt.preventDefault();
+    if (evt.ctrlKey) {
+      const value = scale.value - evt.deltaY / 50;
+      scale.value = Math.max(0.5, value);
+    } else {
+      scroll.x -= evt.deltaX;
+      scroll.y -= evt.deltaY;
+    }
+    draw();
+  });
+});
 </script>
 
 <style lang="scss" module>
--- src/utils/graphic.ts
+++ src/utils/graphic.ts
@@ -3,27 +3,14 @@ interface ISize {
   height: number;
 }
 
-interface IPosition {
-  x: number;
-  y: number;
-}
-
-export function scaleInside(container: ISize, target: ISize, padding = 0): ISize & IPosition {
+export function scaleInside(container: ISize, target: ISize, padding = 0): ISize {
   const ratio = target.width / target.height;
   const canvas = <ISize>{ width: container.width - padding * 2, height: container.height - padding * 2 };
   const matchWidth = <ISize>{ width: canvas.width, height: canvas.width / ratio };
   const matchHeight = <ISize>{ width: ratio * canvas.height, height: canvas.height };
   if (matchWidth.height <= canvas.height) {
-    return {
-      ...matchWidth,
-      x: padding,
-      y: padding + (canvas.height - matchWidth.height) / 2,
-    };
+    return matchWidth;
   } else {
-    return {
-      ...matchHeight,
-      x: padding + (canvas.width - matchHeight.width) / 2,
-      y: padding,
-    };
+    return matchHeight;
   }
 }
