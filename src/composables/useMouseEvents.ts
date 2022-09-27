e58cb8ca08f6da924bb973ec6cfae0bb3f834ead
XiNGRZ <hi@xingrz.me>
2022-09-27 16:42:28 +0800

    Finish dragging on mouse up outside

--- src/composables/useMouseEvents.ts
+++ src/composables/useMouseEvents.ts
@@ -4,7 +4,7 @@ import { useEventListener } from './useEventListener';
 
 export function useMouseEvents(ref: Ref<HTMLElement | undefined>, translate: IPosition, dragging: Ref<boolean>): void {
   useEventListener(ref, 'mousedown', () => dragging.value = true);
-  useEventListener(ref, 'mouseup', () => dragging.value = false);
+  useEventListener(window, 'mouseup', () => dragging.value = false);
   useEventListener(window, 'mousemove', (evt) => {
     if (dragging.value) {
       evt.preventDefault();
